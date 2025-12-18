ba9f9c9ae577c9490b67ff992166e3b14fce95fb
morvra <137384827+morvra@users.noreply.github.com>
2025-12-18 21:13:09 +0900

    不具合修正

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { Card, CardType } from '../types';
 import { formatTimeShort, formatTimestampByPattern } from '../utils';
 import { CardRenderer } from './CardRenderer';
